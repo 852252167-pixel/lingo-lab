7a67337db563c48beb34185ecc9d1d47e0ac3786
852252167-pixel <852252167@qq.com>
2026-06-18 01:04:01 +0800

    Add sw.js

---
+++ sw.js
@@ -0,0 +1,21 @@
+﻿const CACHE = "lingolab-v1";
+const URLS = ["index.html", "manifest.json", "icon.svg"];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(CACHE).then((c) => c.addAll(URLS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((ks) => Promise.all(ks.filter((k) => k !== CACHE).map((k) => caches.delete(k))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cached) => cached || fetch(e.request))
+  );
+});
